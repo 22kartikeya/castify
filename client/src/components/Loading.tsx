0b317b597b3fcd557789dacf6ac6f5e2c95f026d
Kartikeya Gupta <kartikeya.229302253@muj.manipal.edu>
2025-07-31 21:21:42 +0530

    spinner added

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,14 @@
+
+export const Loading = () => {
+    return (
+      <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
+        <div
+          className="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-blue-600 rounded-full"
+          role="status"
+          aria-label="loading"
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+}
