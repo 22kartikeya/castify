b3f8974e42faee77db00d56cfc286cd559ed55a6
Kartikeya Gupta <kartikeya.229302253@muj.manipal.edu>
2025-08-01 19:32:34 +0530

    fixed imports

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const base_backend_url = process.env.BASE_BACKEND_URL;
-export const user_api_url = process.env.USER_API_URL;
-export const admin_api_url = process.env.ADMIN_API_URL;
+export const base_backend_url = import.meta.env.VITE_BASE_BACKEND_URL;
+export const user_api_url = import.meta.env.VITE_USER_API_URL;
+export const admin_api_url = import.meta.env.VITE_ADMIN_API_URL;
